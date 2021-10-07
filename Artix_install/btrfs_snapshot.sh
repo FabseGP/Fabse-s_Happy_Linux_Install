9d44a84257f857e7fd16c0ff88e0e191fdf705a0
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-07 18:02:37 +0200

    Update btrfs_snapshot.sh

--- Artix_install/btrfs_snapshot.sh
+++ Artix_install/btrfs_snapshot.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-NOW=$(date +"%Y-%m-%d_%H:%M:%S")
- 
-if [ ! -e /mnt/backup ]; then
-mkdir -p /mnt/backup
-fi
- 
-cd /
-/bin/btrfs subvolume snapshot / "/mnt/backup/backup_${NOW}"
+
+  NOW=$(date +"%Y-%m-%d_%H:%M:%S")
+  if [ ! -e /mnt/backup ]; then
+    mkdir -p /mnt/backup
+  fi
+  cd /
+  /bin/btrfs subvolume snapshot / "/mnt/backup/backup_${NOW}"
