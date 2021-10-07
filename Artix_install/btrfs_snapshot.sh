203bbbaca9dc32d49940f84f54c5c1f48394ba76
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-07 19:29:27 +0200

    Update btrfs_snapshot.sh

--- Artix_install/btrfs_snapshot.sh
+++ Artix_install/btrfs_snapshot.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-  NOW=$(date +"%Y-%m-%d_%H:%M:%S")
-  if [ ! -e /mnt/backup ]; then
-    mkdir -p /mnt/backup
-  fi
-  cd /
-  /bin/btrfs subvolume snapshot / "/mnt/backup/backup_${NOW}"
+# max_snap is the highest number of snapshots that will be kept
+  max_snap=$((3 - 1))
+
+# time_stamp is the date of snapshots
+  time_stamp=$(date +"%Y-%m-%d_%H:%M:%S")
+
+# Clean up older snapshots:
+  for snapshots_total in $(ls /.snapshots|sort |grep backup|head -n -"$max_snap"); do
+    yay="btrfs subvolume delete /.snapshots/"$snapshots_total""
+    "$yay" > /dev/null
+  done
+
+# Create new snapshot:
+  yay="btrfs subvolume snapshot / /.snapshots/backup-"$time_stamp""
+  "$yay" > /dev/null
