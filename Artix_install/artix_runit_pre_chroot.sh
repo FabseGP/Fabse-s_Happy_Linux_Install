8be9c305b353427f8b34a55c7423380616842429
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-08 18:13:56 +0200

    Update artix_runit_pre_chroot.sh

--- Artix_install/artix_runit_pre_chroot.sh
+++ Artix_install/artix_runit_pre_chroot.sh
@@ -498,7 +498,7 @@
     echo
     print blue "Please have your encryption-password ready "
     echo
-    cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 --use-random "$DRIVE_LABEL_primary"
+    cryptsetup luksFormat --batch-mode --verify-passphrase --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 --use-random "$DRIVE_LABEL_primary"
     cryptsetup open "$DRIVE_LABEL_primary" cryptroot
     echo
   fi
