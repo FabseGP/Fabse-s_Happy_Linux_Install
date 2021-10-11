60edc635a693a33700cea21590759f6fa12b62ca
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-11 17:27:08 +0200

    Update artix_runit_after_chroot.sh

--- Artix_install/artix_runit_after_chroot.sh
+++ Artix_install/artix_runit_after_chroot.sh
@@ -444,17 +444,13 @@ EOF
 EOF
   cd "$BEGINNER_DIR" || exit
   cp btrfs_snapshot.sh /.snapshots # Maximum 3 snapshots stored
+  cp grub-btrfs.sh /.snapshots # Updates GRUB to show snapshots at boot
   ln -s /.snapshots/btrfs_snapshot.sh /etc/cron.daily/btrfs_snapshot.sh
-  chmod u+x /etc/cron.daily/btrfs_snapshot.sh && chmod u+x /.snapshots/btrfs_snapshot.sh
+  ln -s /.snapshots/grub-btrfs.sh /etc/cron.daily/grub-btrfs.sh
+  chmod u+x /etc/cron.daily/* && chmod u+x /.snapshots/*
   sed -i -e "/GRUB_BTRFS_OVERRIDE_BOOT_PARTITION_DETECTION/s/^#//" /etc/default/grub-btrfs/config
   touch /etc/rc.shutdown
-  cat << EOF | tee -a /etc/rc.shutdown > /dev/null # Apparently /etc/rc.shutdown is recreated on first boot, so it has to be entered manually afterwards :(
-# Adding BTRFS-snapshots to grub-menu
-cd /etc/grub.d
-./41_snapshots-btrfs
-grub-mkconfig -o /boot/grub/grub.cfg
-EOF
-sed -i 's/02/13/g' /var/spool/fcron/systab.orig # Taking daily snapshots at 13:00:00
+  sed -i 's/02/13/g' /var/spool/fcron/systab.orig # Taking daily snapshots at 13:00:00
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
